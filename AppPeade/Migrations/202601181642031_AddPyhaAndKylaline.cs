eb5d8e48760aa885471d50c78857119e5158bb83
OleksandraRyshniak <oleksandraryshniak@gmail.com>
2026-01-18 18:46:41 +0200

    18.01

---
+++ AppPeade/Migrations/202601181642031_AddPyhaAndKylaline.cs
@@ -0,0 +1,20 @@
+﻿namespace AppPeade.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPyhaAndKylaline : DbMigration
+    {
+        public override void Up()
+        {
+            CreateIndex("dbo.Kylalines", "PyhaId");
+            AddForeignKey("dbo.Kylalines", "PyhaId", "dbo.Pyhas", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Kylalines", "PyhaId", "dbo.Pyhas");
+            DropIndex("dbo.Kylalines", new[] { "PyhaId" });
+        }
+    }
+}
